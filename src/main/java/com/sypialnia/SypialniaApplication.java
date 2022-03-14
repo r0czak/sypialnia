41b6315019df7d5d391dfa82eac73ba73a007e1f
r0czak <kaczorb.kontakt@gmail.com>
2022-03-15 00:20:30 +0100

    Initial commit

---
+++ src/main/java/com/sypialnia/SypialniaApplication.java
@@ -0,0 +1,12 @@
+package com.sypialnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SypialniaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SypialniaApplication.class, args);
+  }
+}
---
+++ src/test/java/com/sypialnia/SypialniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sypialnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SypialniaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
